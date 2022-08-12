9456f181c58cf06b401c42baf53e6947fbacd8a7
Giossaurus <gihhskt@gmail.com>
2022-08-12 17:12:24 -0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBSsC5PXiI7HD17qoaz-guuRXwI-Nw8qWg",
-    authDomain: "whataboutthis-48e86.firebaseapp.com",
-    databaseURL: "https://whataboutthis-48e86-default-rtdb.firebaseio.com",
-    projectId: "whataboutthis-48e86",
-    storageBucket: "whataboutthis-48e86.appspot.com",
-    messagingSenderId: "640213318880",
-    appId: "1:640213318880:web:ec5bccfd4cc962c7292485"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig);
