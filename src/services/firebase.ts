5b2df23355c9b11295af812e95b81cd848e3d54f
Giossaurus <gihhskt@gmail.com>
2022-08-12 16:56:47 -0300

    firebase added

---
+++ src/services/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBSsC5PXiI7HD17qoaz-guuRXwI-Nw8qWg",
+    authDomain: "whataboutthis-48e86.firebaseapp.com",
+    databaseURL: "https://whataboutthis-48e86-default-rtdb.firebaseio.com",
+    projectId: "whataboutthis-48e86",
+    storageBucket: "whataboutthis-48e86.appspot.com",
+    messagingSenderId: "640213318880",
+    appId: "1:640213318880:web:ec5bccfd4cc962c7292485"
+  };
+
+firebase.initializeApp(firebaseConfig);
