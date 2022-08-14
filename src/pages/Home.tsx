31a233fd5dbaa0616032fda5ddba0d7a3fd28cd5
Giossaurus <gihhskt@gmail.com>
2022-08-14 14:13:31 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
+import illustrationImg from '../assets/images/illustration.svg'
+
 export function Home() {
     return (
-        <h1></h1>
+        <div>
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong> Crie salas de Q&amp;A ao vivo </strong>
+                <p>Tire as dúvidas de sua audiência em tempo real</p>
+            </aside>
+        </div>
     )
 }
