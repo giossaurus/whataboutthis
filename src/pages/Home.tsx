2a78667e541bceb81c482e4356b5ba086fea8578
Giossaurus <gihhskt@gmail.com>
2022-08-14 14:15:56 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,6 @@
 import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
 
 export function Home() {
     return (
@@ -8,6 +10,15 @@ export function Home() {
                 <strong> Crie salas de Q&amp;A ao vivo </strong>
                 <p>Tire as dúvidas de sua audiência em tempo real</p>
             </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="WhatAboutThis" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                </div>
+            </main>
         </div>
     )
 }
